daa2acf86fb684f0ad081c9e030a2da025c5bcbb
ustinovich.yuryi <ustinhtc@gmail.com>
2020-10-25 20:41:08 +0300

    add empty conditional alert

---
+++ src/Components/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Form = () => {
+
+    return (
+        <form>
+            <div className="form-group">
+                <input type="text" placeholder="Введите название заметки" className="form-control"></input>
+            </div>
+        </form>
+    )
+}
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,13 @@
 import React, { Fragment } from 'react';
+import { Form } from '../Components/Form';
+import { Notes } from '../Components/Notes';
 
 export const Home = () => {
+    const notes = new Array(3).fill('').map((_, index) => ({ id: index, title: `Note ${index + 1}` }));
+    
     return <Fragment>
-        <h1>Home Page</h1>
+        <Form />
+        <hr />
+        <Notes notes={notes} />
     </Fragment>
 }
